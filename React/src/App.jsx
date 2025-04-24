0d0b228de1d1be61cef81506e660eb2a58f966ce
ZayraGarcia <chalizgg1602@gmail.com>
2025-04-23 20:15:55 -0500

    Api y React de la tabla comida (web).

---
+++ React/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Comida } from './Components/TablaComida.jsx'
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <Comida/>
+      </div>
+
+    </>
+  );
+}
+
+export default App
