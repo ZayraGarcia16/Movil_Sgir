8c06b75edf78621de144005a776c52084d8d9270
ZayraGarcia <chalizgg1602@gmail.com>
2025-04-23 22:29:00 -0500

    Api y React de la tabla actividad (web).

---
+++ React/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Actividad } from './Components/TablaActividad.jsx'
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <Actividad/>
+      </div>
+
+    </>
+  );
+}
+
+export default App
